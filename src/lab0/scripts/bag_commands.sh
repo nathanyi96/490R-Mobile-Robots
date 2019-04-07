3600bc49987e2d5dc0acb1ed18d914dde5c43f2e
Quiz <1003301648@qq.com>
2019-04-06 23:45:41 -0700

    finish ~3.1.3

--- src/lab0/scripts/bag_commands.sh
+++ src/lab0/scripts/bag_commands.sh
@@ -1,2 +1,2 @@
-rosbag record -b 1 -o test.bag /vesc/low_level/ackermann_cmd_mux/input/teleop
+rosbag record -b 1 -o test.bag /vesc/low_level/ackermann_cmd_mux/input/teleop /initialpose
 rosbag play --hz 20 /vesc/low_level/ackermann_cmd_mux/input/teleop
--- src/lab0/src/bag_follower.py
+++ src/lab0/src/bag_follower.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python
 import rospy
 import rosbag
+from geometry_msgs.msg import PoseWithCovarianceStamped
 from ackermann_msgs.msg import AckermannDriveStamped
 
 if __name__ == '__main__':
     rospy.init_node('bag_follower', anonymous=True)
+    init_pub = rospy.Publisher('/initialpose', PoseWithCovarianceStamped, queue_size=1)
+
+    teleop_pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped, queue_size=1)
 
     bag_file = rospy.get_param("~bag_file")
     rospy.loginfo("bag file: " + str(bag_file))
     bag = rosbag.Bag(bag_file)
+    
+    init_msg = None
     bag_data = []
-    for topic, msg, t in bag.read_messages(topics=['/initalpose', '/vesc/low_level/ackermann_cmd_mux/input/teleop']):
-        bag_data.append(msg)
-        rospy.loginfo('read message on topic ' + topic)
+    for topic, msg, t in bag.read_messages(topics=['/initialpose', '/vesc/low_level/ackermann_cmd_mux/input/teleop']):
+        if topic == '/initialpose':
+            rospy.loginfo('read message on topic ' + topic)
+            rospy.loginfo('msg: ' + str(msg))
+            init_msg = msg
+        else:
+            bag_data.append(msg)
 
-    teleop_pub = rospy.Publisher('/vesc/high_level/ackermann_cmd_mux/input/nav_0', AckermannDriveStamped, queue_size=1)
-    rate = rospy.Rate(2)
-    #while not rospy.is_shutdown():
+    rate = rospy.Rate(20)
+
+    init_pub.publish(init_msg)
     for msg in bag_data:
         if not rospy.is_shutdown():
             teleop_pub.publish(msg)
