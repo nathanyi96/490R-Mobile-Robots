5ad7c42e9ce9cf1f0baf6a4f7c92bd0111b0badf
Henry Hung <billhung56@gmail.com>
2019-04-17 19:51:39 -0700

    update

--- build/lab0/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robots/490R_Projects/src/lab0
-# Build directory: /home/robots/490R_Projects/build/lab0
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
